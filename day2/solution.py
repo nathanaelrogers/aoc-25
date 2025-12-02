b4c2e72270513e18dcc539da66539f2c0f3c2470
Nathanael Rogers <rogersnw@amazon.co.uk>
2025-12-02 19:20:49 +0000

    day 2 done

---
+++ day2/solution.py
@@ -0,0 +1,59 @@
+from itertools import batched
+
+def parse_range(string):
+    start, end = string.split('-')
+    return (int(start), int(end))
+
+with open('input.txt') as file:
+    ranges = map(parse_range, file.read().split(','))
+
+# Part 1
+
+res = []
+
+for start, end in ranges:
+    for id in map(str, range(start, end+1)):
+        id_len = len(id)
+
+        if id_len % 2 != 0:
+            continue
+
+        if id[id_len // 2:] != id[:id_len // 2]:
+            continue
+        
+        res.append(int(id))
+
+print('part 1:', sum(res))
+
+#  Part 2
+
+res = []
+
+def check_invalid_id(id):
+    win_size = len(id) // 2
+
+    while win_size > 0:
+        id_invalid = True
+        pattern = id[:win_size]
+
+        for slice in batched(id[win_size:], n=win_size):
+            slice = ''.join(slice)
+
+            if slice != pattern:
+                id_invalid = False
+                break
+
+        if id_invalid:
+            return True
+        
+        win_size -= 1
+    
+    return False
+
+for start, end in ranges:
+    for id in map(str, range(start, end+1)):
+        if check_invalid_id(id):
+            res.append(int(id))
+
+print('part 2:', sum(res))
+        
